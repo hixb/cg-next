6abe59b366a9f24f1a4a7dcc9259e0256d278d86
hixb <Hello16369@163.com>
2024-01-20 15:46:35 +0800

    chore: structural cleanup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
+import '@/styles/tailwind.scss'
 import '@arco-design/web-react/dist/css/arco.css'
 
 const inter = Inter({ subsets: ['latin'] })
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react'
 import clsx from 'clsx'
-import { Button, TimePicker } from '@arco-design/web-react'
+import { Button } from '@arco-design/web-react'
 
 export default function Home() {
   const [isPlaying, setIsPlaying] = React.useState(false)
@@ -10,7 +10,6 @@ export default function Home() {
   return (
     <main className={clsx('text-red-100 text-lg transition-all delay-150 cursor-pointer', { 'text-3xl': isPlaying })} onClick={() => setIsPlaying(!isPlaying)}>
       <Button type="primary">Hello Arco</Button>
-      <TimePicker style={{ width: 194 }}></TimePicker>
     </main>
   )
 }
