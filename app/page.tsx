47ea2ebdbeaad5f6d32d72e07049674277f9e22f
hixb <Hello16369@163.com>
2024-01-20 15:29:46 +0800

    chore: init

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import React from 'react'
+import clsx from 'clsx'
+import { Button, TimePicker } from '@arco-design/web-react'
+
+export default function Home() {
+  const [isPlaying, setIsPlaying] = React.useState(false)
+
+  return (
+    <main className={clsx('text-red-100 text-lg transition-all delay-150 cursor-pointer', { 'text-3xl': isPlaying })} onClick={() => setIsPlaying(!isPlaying)}>
+      <Button type="primary">Hello Arco</Button>
+      <TimePicker style={{ width: 194 }}></TimePicker>
+    </main>
+  )
+}
